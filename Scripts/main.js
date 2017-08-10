9b86750e8cfc9f2cefdabf913562cd5247434186
mikojimnz <jimenezmikog@gmail.com>
2017-08-09 21:29:29 -0400

    Updated UI & Set new dates

--- Scripts/main.js
+++ Scripts/main.js
@@ -9,36 +9,36 @@ function update() {
         var count = "";
 
         if (day == 1) {
-            count += day + " Day, ";
+            count += day + " Day ";
         } else if (day <= 0) {
             count += "";
         } else {
-            count += day + " Days, ";
+            count += day + " Days ";
         }
 
-        if (hour == 1) {
-            count += hour + " Hour, ";
-        } else if (hour <= 0) {
-            count += "";
-        } else {
-            count += hour + " Hours, ";
-        }
-
-        if (min == 1) {
-            count += min + " Minute, ";
-        } else if (min <= 0) {
-            count += "";
-        } else {
-            count += min + " Minutes, ";
-        }
-
-        if (sec == 1) {
-            count += sec + " Second";
-        } else if (sec <= 0) {
-            count += "0 Seconds";
-        } else {
-            count += sec + " Seconds";
-        }
+//        if (hour == 1) {
+//            count += hour + " Hour, ";
+//        } else if (hour <= 0) {
+//            count += "";
+//        } else {
+//            count += hour + " Hours, ";
+//        }
+//
+//        if (min == 1) {
+//            count += min + " Minute, ";
+//        } else if (min <= 0) {
+//            count += "";
+//        } else {
+//            count += min + " Minutes, ";
+//        }
+//
+//        if (sec == 1) {
+//            count += sec + " Second";
+//        } else if (sec <= 0) {
+//            count += "0 Seconds";
+//        } else {
+//            count += sec + " Seconds";
+//        }
 
         count += " Left";
 
@@ -47,14 +47,14 @@ function update() {
         }
 
         return count;
-    }
+    };
 
-    document.getElementById("r").innerHTML = calTime("10/25/2016 13:00:00");
-    document.getElementById("y").innerHTML = calTime("11/16/2016 12:00:00");
-    document.getElementById("g").innerHTML = calTime("12/06/2016 12:00:00");
-    document.getElementById("b").innerHTML = calTime("01/11/2017 12:00:00");
-    document.getElementById("pu").innerHTML = calTime("02/07/2017 12:00:00");
-    document.getElementById("pi").innerHTML = calTime("03/07/2017 12:00:00");
+    document.getElementById("r").innerHTML = calTime("11/2/2017 13:00:00");
+    document.getElementById("b").innerHTML = calTime("11/16/2017 12:00:00");
+    document.getElementById("g").innerHTML = calTime("12/07/2017 12:00:00");
+    document.getElementById("y").innerHTML = calTime("01/11/2018 12:00:00");
+    document.getElementById("pu").innerHTML = calTime("02/08/2018 12:00:00");
+    document.getElementById("pi").innerHTML = calTime("03/08/2018 12:00:00");
     setTimeout(update, 1000);
 }
 
